3192da4c5457dbd393bc541ac322a1285dc9bab7
bapbop1 <hungnx96@gmail.com>
2018-05-27 13:13:36 +0700

    manage_categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,7 +3,7 @@ module Admin
     before_action :load_category, except: %i(index new create)
 
     def index
-      @categories = Category.paginate(page: params[:page], per_page: 3)
+      @categories = Category.paginate page: params[:page], per_page: 3
     end
 
     def show; end
@@ -17,7 +17,7 @@ module Admin
     def create
       @category = Category.create category_pamrams
       if @category.save
-        flash[:success] = t ".create_success"
+        flash[:success] = t "create_success"
         redirect_to admin_categories_url
       else
         render :new
@@ -26,7 +26,7 @@ module Admin
 
     def update
       if @category.update category_pamrams
-        flash[:success] = t ".update_success"
+        flash[:success] = t "update_success"
         redirect_to admin_categories_url
       else
         render :edit
@@ -35,9 +35,9 @@ module Admin
 
     def destroy
       if @category.destroy
-        flash[:success] = "xoa thanh cong"
+        flash[:success] = t "delete_success"
       else
-        flash[:errors] = "xoa that bai"
+        flash[:errors] = t "delete_fail"
       end
       redirect_to admin_categories_url
     end
@@ -46,6 +46,9 @@ module Admin
 
     def load_category
         @category = Category.find_by id: params[:id]
+        return if @category
+        flash[:danger] = t "category_not_found"
+        redirect_to admin_categories_url
     end
 
     def category_pamrams
